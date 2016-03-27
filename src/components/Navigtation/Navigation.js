1054ae1c6db025c4f4990328de8245c6bd21890b
Dave Edelhart <dave@wonderlandlabs.com>
2016-03-26 18:25:59 -0700

    further compounding navigation menus

--- src/components/Navigtation/Navigation.js
+++ src/components/Navigtation/Navigation.js
@@ -7,106 +7,105 @@ import closeDrawer from '../../helpers/closeDrawer';
 
 @connect((state) => state, {})
 export default class TopNavigation extends Component {
-  static propTypes = {
-    user: PropTypes.object,
-    navstyles: PropTypes.object,
-    pushState: PropTypes.func.isRequired,
-    logout: PropTypes.func.isRequired,
-    drawer: PropTypes.number
-  };
-  static contextTypes = {
-    router: React.PropTypes.object
-  }
-
-  componentDidMount () {
-    console.log('this.refs: ', this.refs);
-  }
-
-  handleLogout = (event) => {
-    event.preventDefault();
-    this.props.logout();
-  };
-
-  toLogin () {
-    this.props.pushState('/login');
-    if (this.props.drawer) {
-      closeDrawer.close();
+    static propTypes = {
+        user: PropTypes.object,
+        navstyles: PropTypes.object,
+        pushState: PropTypes.func.isRequired,
+        logout: PropTypes.func.isRequired,
+        drawer: PropTypes.number
+    };
+    static contextTypes = {
+        router: React.PropTypes.object
     }
-  }
 
-  render () {
-    const navstyles = this.props.navstyles;
-    const localstyles = require('./Navigation.scss');
-    const styles = Object.assign({}, navstyles, localstyles);
-    const user = this.props.user;
-    const buttonClass = this.props.drawer ? '' : 'mdl-layout--large-screen-only';
-    const title = (
-      <IndexLink to="/" className={styles['brand-link']} activeStyle={{color: '#33e0ff'}}>
-        <div className={styles['brand-link__brand']}/>
-        <span className={styles['brand-link__brand-text']}>{config.app.title}</span>
-      </IndexLink>
-    );
+    componentDidMount() {
+        console.log('this.refs: ', this.refs);
+    }
 
-    const go = (link) => () => {
-      this.props.pushState(link);
-      if (this.props.drawer) {
-        closeDrawer.close();
-      }
+    handleLogout = (event) => {
+        event.preventDefault();
+        this.props.logout();
     };
 
-    const makeLink = (link, text) => (
-      <div key={`${link}-button`} className={buttonClass}>
-        <Button raised={this.context.router.isActive(link)} onClick={go(link)}>
-          {text}
-        </Button>
-      </div>);
-
-    const children = [
-      makeLink('/widgets', 'Widgets'),
-      makeLink('/survey', 'Survey'),
-      makeLink('/about', 'About Us')
-    ];
-
-    if (user) {
-      children.unshift(
-        makeLink('/chat', 'Chat')
-      );
-      children.push(
-        <div key="logout-button"><Button to="/logout">
-          <div className="logout-link" onClick={this.handleLogout.bind(this)}>
-            Logout
-          </div>
-        </Button></div>);
-
-      children.push(
-        <div key="userid" className={buttonClass}>Logged in as&nbsp;
-          <strong>{user.name}</strong>.</div>);
-
-      children.push(
-        <div key="github-button" className={buttonClass}>
-          <Button target="_blank" title="View on Github"
-                  href="https://github.com/erikras/react-redux-universal-hot-example">
-            <i className="fa fa-github"/>
-          </Button></div>);
-    } else {
-      children.push(
-        makeLink('/login', 'Log In')
-      );
+    toLogin() {
+        this.props.pushState('/login');
+        if (this.props.drawer) {
+            closeDrawer.close();
+        }
     }
 
-    return this.props.drawer ? (
-      <Drawer>
-        <Navigation>
-          {children}
-          <p>I am drawer</p>
-        </Navigation>
-      </Drawer>
-    ) : (
-      <Header title={title}>
-        <Navigation>
-          {children}
-        </Navigation>
-      </Header>
-    );
-  }
+    render() {
+        const navstyles = this.props.navstyles;
+        const localstyles = require('./Navigation.scss');
+        const styles = Object.assign({}, navstyles, localstyles);
+        const user = this.props.user;
+        const buttonClass = this.props.drawer ? '' : 'mdl-layout--large-screen-only';
+        const title = (
+          <IndexLink to="/" className={styles['brand-link']} activeStyle={{color: '#33e0ff'}}>
+              <div className={styles['brand-link__brand']}/>
+              <span className={styles['brand-link__brand-text']}>{config.app.title}</span>
+          </IndexLink>
+        );
+
+        const go = (link) => () => {
+            this.props.pushState(link);
+            if (this.props.drawer) {
+                closeDrawer.close();
+            }
+        };
+
+        const makeLinkButton = (link, text, comms) => {
+            const raised = this.context.router.isActive(link);
+            const linkStyle = {};
+            if ((!this.props.drawer) && raised) {
+                linkStyle.color = 'white';
+            }
+            return ( <div key={`${link.replace(/^\\/, '')}-button`} className={buttonClass} {...comms}>
+                <Button raised={raised} style={linkStyle} onClick={go(link)}>
+                    {text}
+                </Button>
+            </div>);
+        };
+
+        const makeLink = (link, text) => {
+            return makeLinkButton(link, text, {onClick: go(link)});
+        };
+
+        const children = [
+            makeLink('/widgets', 'Widgets'),
+            makeLink('/survey', 'Survey'),
+            makeLink('/about', 'About Us')
+        ];
+
+        if (user) {
+            children.unshift(
+              makeLink('/chat', 'Chat')
+            );
+            children.push(
+              <div key="userid" className={buttonClass}>Logged in as&nbsp;
+                  <strong>{user.name}</strong>.</div>);
+            children.push(
+              makeLinkButton('/logout', 'Log Out', {onClick: this.handleLogout.bind(this)} ));
+
+        } else {
+            children.push(
+              makeLink('/login', 'Log In')
+            );
+        }
+
+        return this.props.drawer ? (
+          <Drawer>
+              <Navigation>
+                  {children}
+                  <p>I am drawer</p>
+              </Navigation>
+          </Drawer>
+        ) : (
+          <Header title={title}>
+              <Navigation className={styles.navigation}>
+                  {children}
+              </Navigation>
+          </Header>
+        );
+    }
 }
